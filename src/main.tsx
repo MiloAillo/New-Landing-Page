010852ee96977902eb40cba0256fa9d1e0b7b174
mischikomoe <panuwucake@gmail.com>
2026-06-06 15:02:03 +0700

    chore: removed locomotive scroll

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import mainRouter from './routes/MainRouter'
-import LocomotiveScroll from 'locomotive-scroll'
-
-const locomotive = new LocomotiveScroll()
-
-addEventListener("resize", () => locomotive.resize())
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode> 
